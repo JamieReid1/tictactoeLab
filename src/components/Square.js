81c4584671adc5572ae679ec223a74276a5bb926
Nadja Kelly <nmfort-info@yahoo.de>
2019-03-06 12:27:35 +0000

    set-up board square class

---
+++ src/components/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Square extends React.Component{
+    render(){
+        return(
+            <button className="square">
+            
+            </button>
+        );
+    }
+}
+
+
+
+
+export default Square;
